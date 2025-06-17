4471fc116d13246cf9c2ae6ba1d8dd49cd6c8bf8
kenshin0413 <kenshin1234569@gmail.com>
2025-06-17 19:33:54 +0900

    最初

---
+++ counter-app/counter-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  counter-app
+//
+//  Created by miyamotokenshin on R 7/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ counter-app/counter-app/counter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  counter_appApp.swift
+//  counter-app
+//
+//  Created by miyamotokenshin on R 7/06/17.
+//
+
+import SwiftUI
+
+@main
+struct counter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
