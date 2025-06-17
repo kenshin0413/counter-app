dc4d692a1d8637b34338e3425edf6b3cf06285d7
kenshin0413 <kenshin1234569@gmail.com>
2025-06-17 20:55:03 +0900

    プロジェクトの変更

--- counter-app/counter-app/ContentView.swift
+++ counter-app/counter-app/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var count: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            VStack {
+                Text("\(count)")
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack {
+                    Button(
+                        action: {
+                            count -= 1
+                        },label: {
+                            Image(systemName: "minus")
+                                .frame(width: 150, height: 50)
+                                .background(Color(.systemGray4))
+                                .cornerRadius(10)
+                        }
+                    )
+                    
+                    Button(
+                        action: {
+                            count += 1
+                        },label: {
+                            Image(systemName: "plus")
+                                .frame(width: 150, height: 50)
+                                .background(Color(.systemGray4))
+                                .cornerRadius(10)
+                        }
+                    )
+                }
+            }
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
