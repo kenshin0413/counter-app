e67ad9aefd5a02bf23320f7ae12d3e3d95de7c59
kenshin0413 <kenshin1234569@gmail.com>
2025-06-19 20:11:32 +0900

    モーダル遷移と変数名変更

--- counter-app/counter-app/ContentView.swift
+++ counter-app/counter-app/ContentView.swift
@@ -8,9 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State var count: Int = 0
-    
+    @State var isShowingModal = false
     var body: some View {
         NavigationStack {
             VStack(spacing: 20) {
@@ -22,46 +21,47 @@ struct ContentView: View {
                     Button(
                         action: {
                             count -= 1
-                        },label: {
+                        }, label: {
                             Image(systemName: "minus")
                                 .frame(width: 150, height: 50)
                                 .background(Color(.systemGray4))
                                 .cornerRadius(10)
-                        }
-                    )
+                        })
                     
                     Button(
                         action: {
                             count += 1
-                        },label: {
+                        }, label: {
                             Image(systemName: "plus")
                                 .frame(width: 150, height: 50)
                                 .background(Color(.systemGray4))
                                 .cornerRadius(10)
-                        }
-                    )
+                        })
                 }
                 
                 Button(
                     action: {
                         count = 0
-                    },label: {
+                    }, label: {
                         Text("リセット")
                             .foregroundStyle(Color.white)
                             .frame(width: 300, height: 50)
                             .background(Color.blue)
                             .cornerRadius(10)
-                    }
-                )
+                    })
                 
-                NavigationLink {
+                Button(
+                    action: {
+                        isShowingModal = true
+                    }, label: {
+                        Text("設定")
+                            .foregroundStyle(Color.white)
+                            .frame(width: 300, height: 50)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    })
+                .sheet(isPresented: $isShowingModal) {
                     SettingView()
-                } label: {
-                    Text("設定")
-                        .foregroundStyle(Color.white)
-                        .frame(width: 300, height: 50)
-                        .background(Color.blue)
-                        .cornerRadius(10)
                 }
             }
         }
--- counter-app/counter-app/SettingView.swift
+++ counter-app/counter-app/SettingView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct SettingView: View {
     
-    @State var number = ""
+    @State var maxCount = ""
     
     var body: some View {
         HStack {
-                Text("カウンターの上限値:")
-                TextField("上限値を入力してください", text: $number)
+            Text("カウンターの上限値:")
+            TextField("上限値を入力してください", text: $maxCount)
                 .frame(width: 180)
-            }
         }
     }
+}
 
 #Preview {
     SettingView()
